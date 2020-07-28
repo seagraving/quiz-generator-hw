ae4d1f129f6c5f3a0c3eea79cd7858d579fa1270
Sarah Seagraves <67175494+seagraving@users.noreply.github.com>
2020-07-27 22:33:25 -0500

    fixed choices for questions

--- assets/script.js
+++ assets/script.js
@@ -1,54 +1,50 @@
 // questions to be asked
 var questions = [
   {
-      title: "What is Javascript?",
-      choices: ["A scripting or programming language that lets you incorporate more complex features on web pages", 
-                  "Manual about different coffee varietes", 
-                  "A style sheet language used to describe how HTML elements will be displayed on screen"],
-      answer: "A scripting or programming language that lets you incorporate more complex features on web pages"
+      question: "What is Javascript?",
+      choice1: "A scripting or programming language that lets you incorporate more complex features on web pages",
+      choice1: "Manual about different coffee varietes", 
+      choice3:"A style sheet language used to describe how HTML elements will be displayed on screen",
+      answer: 1,
 
   },
 
   {
-      title: "What is CSS?",
-      choices: ["Some new type of snack", 
-                  "A family of different fonts", 
-                  "A style sheet language used to describe how HTML elements will be displayed on screen"],
-      answer: "A style sheet language used to describe how HTML elements will be displayed on screen"
+      question: "What is CSS?",
+      choice1: "Some new type of snack", 
+      choice2: "A family of different fonts", 
+      choice3: "A style sheet language used to describe how HTML elements will be displayed on screen",
+      answer: 3,
 
   },
 
   {
-      title: "The most common types of data types do NOT include:",
-      choices: ["booleans", 
-                  "popups", 
-                  "strings"],
-      answer: "popups"
+      question: "The most common types of data types do NOT include:",
+      choice1: "booleans", 
+      choice2: "popups", 
+      choice3: "strings",
+      answer: 2,
 
   },
 
   {
-      title: "Which symbol is used to represent an <id> in CSS?",
-      choices: ["#", "+", "()"],
-      answer: "#"
+      question: "Which symbol is used to represent an <id> in CSS?",
+      choice1: "#", 
+      choice2: "+", 
+      choice3: "()",
+      answer: 1,
 
   },
 
   {
-      title: "What is a useful tool for debugging problems on the webpage?",
-      choices: ["creator.log", "console.log", "help.log"],
-      answer: "console.log"
+      question: "What is a useful tool for debugging problems on the webpage?",
+      choice1: "creator.log", 
+      choice2: "console.log", 
+      choice3: "help.log",
+      answer: 2,
 
   },
 
-  {
-      title: "What is div in HTML?",
-      choices: ["A tag that defines a division or section in HTML", 
-                  "Calls out a function in Javascript", 
-                  "Creates a new paragraph"],
-      answer: "A tag that defines a division or section in HTML"
-
-  },
 ];
 
 //setting values for timer
@@ -110,7 +106,7 @@ function timeLeft() {
 // get questions for the quiz
 function getQuestion() {
   var questionAsked = questions[currentQuestionIndex];
-  questionArea.innerText = questionAsked.title
+  questionArea.innerText = questionAsked.question
 
   questionAsked.choices.forEach(function(choice, i){
     var choiceEl = document.createElement("button");
